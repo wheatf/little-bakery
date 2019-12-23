af1de1675e2dc92aa332d7e591b91980cd85500f
wheatf <jimmy_wong_mun_keat@hotmail.com>
2019-12-23 20:30:17 +0800

    Test

--- datastores/bread-datastore.js
+++ datastores/bread-datastore.js
@@ -18,5 +18,23 @@ module.exports = {
      */
     find: async function(id) {
         return await breadModel.findById(id);
+    },
+
+    /**
+     * Get a bread from the database.
+     * 
+     * @param {String} name - The name of the bread to search for.
+     */
+    findByName: async function(name) {
+        return await breadModel.find({name: name});
+    },
+
+    /**
+     * Get a bread from the database.
+     * 
+     * @param {String} description - The description of the bread to search for.
+     */
+    findByDescription: async function(description) {
+        return await breadModel.find({description: description});
     }
 }
