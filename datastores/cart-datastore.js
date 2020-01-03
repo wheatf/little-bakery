8fd4561299b963566b9fc8d41c2cb5f4e4bfc2de
wheatf <jimmy_wong_mun_keat@hotmail.com>
2020-01-03 20:55:54 +0800

    Added registration and login capabilities

--- datastores/cart-datastore.js
+++ datastores/cart-datastore.js
@@ -11,27 +11,7 @@ module.exports = {
      * @param {Mongoose.SchemaTypes.ObjectId} id - The _id of the user to search for.
      */
     findByUserId: async function(id) {
-        return await cartModel.aggregate([
-            {
-                $lookup: {
-                    from: userModel.collection.name,
-                    localField: 'user',
-                    foreignField: '_id',
-                    as: 'user'
-                }
-            },
-            {
-                $match: { 'user._id' : id }
-            },
-            {
-                $lookup: {
-                    from: productModel.collection.name,
-                    localField: 'products',
-                    foreignField: '_id',
-                    as: 'products'
-                }
-            }
-        ]);
+        return await cartModel.find({user: id}).populate('product').exec();
     },
 
     /**
@@ -40,13 +20,12 @@ module.exports = {
      * @param {cartModel} cart - The cart to add to database.
      */
     add: async function(cart) {
-        // Find current user's carts
-        let userCarts = await this.findByUserId(cart.user);
+
         // Check if item exists in cart.
-        let cartItem = userCarts.find(item => item.product == cart.product);
+        let cartItem = await cartModel.findOne({user: cart.user, product: cart.product});
 
         if (cartItem) { // Item exists, add to quantity instead.
-            cartItem.quantity += cart.quantity;
+            cartItem.quantity += parseInt(cart.quantity);
             cartItem.save();
         } else {
             // Item doesn't exists, insert to database.
--- models/user.js
+++ models/user.js
@@ -26,9 +26,9 @@ const userSchema = new Schema({
     username: String,
     password: {
         type: String,
-        get: function(password) { // Calls the following method when retrieving the password.
-            return Buffer.from(password, 'base64'); // Decode Base64; the encoding used for storing password in the database.
-        },
+        // get: function(password) { // Calls the following method when retrieving the password.
+        //     return Buffer.from(password, 'base64'); // Decode Base64; the encoding used for storing password in the database.
+        // },
         set: function(password) { // Calls the following method when setting the password.
             return crypto.createHash('sha256').update(password).digest('base64'); // Hash the password using SHA256, then encode using Base64.
         }
