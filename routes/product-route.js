470009b823c09ebb6c0d024b496124ea94124632
yancq98 <yancq98@gmail.com>
2020-01-30 19:17:19 +0800

    added search feature

--- routes/product-route.js
+++ routes/product-route.js
@@ -6,4 +6,10 @@ const router = express.Router();
 // Clients wants to go to a specific product
 router.get('/:productId', productController.productPage);
 
+// Clients wants to go search page
+router.get('/search/:valueToSearch', productController.searchPage);
+
+// Client wants to search for product by product name
+router.post('/search', productController.search);
+
 module.exports = router;
